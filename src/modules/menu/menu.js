7c3fb76f85c1290d9c545c909af04cbbfcc38eb1
Parham Saravani <parhamsaravani83@gmail.com>
2026-07-05 02:54:33 +0330

    mobile menu is compeleted

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -1,13 +1,22 @@
 const menuItems = document.querySelectorAll(".menu-items");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuOpenBtn = document.querySelector(".mobile-menu-opener");
+const mobileMenuCloserBtn = document.querySelector(".close-mobile-menu");
 
 const activeClickedMenuItem = (event) => {
-    const currentActiveItem = event.target.closest('.menu-items.active')
-    if(currentActiveItem) return;
-    document.querySelector('.menu-items.active').classList.remove('active')
-    event.target.classList.add('active')
+  const currentActiveItem = event.target.closest(".menu-items.active");
+  if (currentActiveItem) return;
+  document.querySelector(".menu-items.active").classList.remove("active");
+  event.target.classList.add("active");
 };
-
+const showModileMenu = () => {
+  mobileMenu.classList.remove('hide')
+}
+const hideMobileMenu = () => {
+  mobileMenu.classList.add('hide')
+}
 menuItems.forEach((item) => {
   item.addEventListener("click", activeClickedMenuItem);
 });
-
+mobileMenuOpenBtn.addEventListener('click', showModileMenu)
+mobileMenuCloserBtn.addEventListener('click', hideMobileMenu)
--- src/modules/theme/theme.js
+++ src/modules/theme/theme.js
@@ -1,14 +1,19 @@
 const toggleInput = document.querySelector(".theme-checkbox");
+const mobileToggleInput = document.querySelector(".mobile-menu-theme-checkbox");
 const themeSwith = document.querySelector(".theme-toggle");
+const mobileThemeSwith = document.querySelector(".mobile-theme-toggle");
 
 const applyTheme = () => {
   const themeStatus = JSON.parse(localStorage.getItem("isDarkMode")) || false;
   if (themeStatus) {
     themeSwith.classList.add("theme-active");
+    mobileThemeSwith.classList.add("mobile-theme-active");
     darkTheme();
     toggleInput.checked = true;
+    mobileToggleInput.checked = true;
   } else {
     toggleInput.checked = false;
+    mobileToggleInput.checked = false;
     lightTheme;
   }
 };
@@ -16,9 +21,11 @@ const changeTheme = (event) => {
   if (toggleInput.checked) {
     darkTheme();
     themeSwith.classList.add("theme-active");
+    mobileThemeSwith.classList.add("mobile-theme-active");
   } else {
     lightTheme();
     themeSwith.classList.remove("theme-active");
+    mobileThemeSwith.classList.remove("mobile-theme-active");
   }
 };
 const darkTheme = () => {
@@ -34,6 +41,19 @@ const saveThemeInLocalStorage = (theme) => {
   localStorage.setItem("isDarkMode", theme);
 };
 
+const mobileThemeHandler = () => {  
+  if (mobileToggleInput.checked) {
+    darkTheme();
+    mobileThemeSwith.classList.add("mobile-theme-active");
+    themeSwith.classList.add("theme-active");
+  } else {
+    lightTheme();
+    mobileThemeSwith.classList.remove("mobile-theme-active");
+    themeSwith.classList.remove("theme-active");
+  }
+};
+
 toggleInput.addEventListener("click", changeTheme);
+mobileToggleInput.addEventListener("click", mobileThemeHandler);
 
 export default applyTheme;
